33c274e2587e4f4e7f5958058791b03af0fd10a3
brownster <marcsmobile.brown@gmail.com>
2023-11-12 00:27:20 +0000

    update main.py and index.html

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from workbook_exporter import process_exporter
 from yaml_format_fixer import process_yaml_file, process_diff_to_html
 from html_bookmark_generator import filter_exporters, generate_bookmarks_html
 from workbook_importer import yaml_to_csv
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/tmp'
@@ -27,25 +28,55 @@ def index():
             file.save(filepath)
 
             if operation == 'super-putty':
-                # Process for SuperPutty XML Generation
                 group_name = request.form['group-name']
                 column_name = request.form['column-name']
                 match_value = request.form['match-value']
                 df = pd.read_excel(filepath)
                 xml_content = generate_putty_sessions_xml(df, group_name, column_name, match_value)
-
-                # Send the XML content as a downloadable file
                 mem = io.BytesIO()
                 mem.write(xml_content.encode('utf-8'))
                 mem.seek(0)
                 return send_file(mem, as_attachment=True, filename="putty_sessions.xml")
 
-            elif operation == 'yaml-to-csv':
+            elif operation == 'workbook-export':
+                # Logic for Workbook Export
+                existing_yaml = request.files.get('existing_yaml')
+                default_listen_port = request.form.get('default_listen_port')
+                output = process_exporter(filepath, existing_yaml, default_listen_port)
+                return output
+
+            elif operation == 'workbook-import':
                 # Process YAML to CSV
                 output_filename = filename.rsplit('.', 1)[0] + '.csv'
                 output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
                 yaml_to_csv(filepath, output_filepath)
                 return send_file(output_filepath, as_attachment=True)
+
+            elif operation == 'rdp-man':
+                # Logic for RDPMan RDG Generation
+                group_name = request.form['group_name']
+                column_name = request.form['column_name']
+                match_value = request.form['match_value']
+
+                wb = load_workbook(filename=filepath)
+                sheet = wb.active
+                df = pd.read_excel(filepath, engine='openpyxl')
+
+                # Check if the column exists
+                if column_name not in df.columns:
+                    flash(f"The column '{column_name}' does not exist in the Excel file.")
+                    return redirect(url_for('index'))
+
+                # Additional processing based on the RDPMan script...
+                matched_df = df[df[column_name].astype(str).str.strip() == match_value.strip()]
+                rdg_content = generate_rdg(matched_df, group_name)
+
+                # Send the RDG content as a downloadable file
+                mem = io.BytesIO()
+                mem.write(rdg_content.encode('utf-8'))
+                mem.seek(0)
+                processed_filename = f"processed_{filename.rsplit('.', 1)[0]}.rdg"
+                return send_file(mem, as_attachment=True, filename=processed_filename)
 
             elif operation == 'bookmark':
                 # Process for HTML Bookmark Generation
@@ -59,8 +90,8 @@ def index():
                 mem.seek(0)
                 return send_file(mem, as_attachment=True, filename="bookmarks.html")
 
-            # Add additional elif blocks for other operations...
-        
+               # Additional operations can be handled here...
+
         else:
             flash('Invalid file format')
             return redirect(request.url)
